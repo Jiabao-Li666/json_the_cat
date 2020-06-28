0f2fdd456403d5d0d360b1292fe6f2dafe2e79e8
Jiabao Li <hunterxhunterzhiqiya@126.com>
2020-06-28 19:02:58 +0000

    add test file for breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,16 @@
 const request = require('request');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + process.argv[2], (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-  } else if (body.length === 2) {
-    console.log('Breed Not Found!');
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0]["description"]);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else if (body.length === 2) {
+      callback('Breed Not Found!', null);
+    } else {
+      const data = JSON.parse(body);
+      callback(null, data[0]["description"]);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
